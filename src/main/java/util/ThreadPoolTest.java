a9416e5b48485635fb062cde80f6d445e720ccb4
xiaozhi <xiaozigbd@yeah.net>
2017-12-28 22:06:55 +0800

    为啥报错

--- src/main/java/util/ThreadPoolTest.java
+++ src/main/java/util/ThreadPoolTest.java
@@ -1,6 +1,6 @@
 package util;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
+//import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
@@ -16,16 +16,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class ThreadPoolTest {
     public static void main(String[] a){
-        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("thread-downloadContracts-%d").build();
-        ExecutorService pool = new ThreadPoolExecutor(5, 200, 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingDeque<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
-        for(int i=0; i< 3; i++){
-            DownloadOneContract downloadOneContract = new DownloadOneContract("",
-                    "https://opentest.haiermoney.com:18980/sign/index/down_mod/10d3b25d1e86684ad9f673bfc7096e77",
-                    "D:\\localFile\\contracts\\"+i+"-hello.pdf");
-            pool.execute(downloadOneContract);
-        }
-        pool.shutdown();
+//        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("thread-downloadContracts-%d").build();
+//        ExecutorService pool = new ThreadPoolExecutor(5, 200, 0L, TimeUnit.MILLISECONDS,
+//                new LinkedBlockingDeque<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+//        for(int i=0; i< 3; i++){
+//            DownloadOneContract downloadOneContract = new DownloadOneContract("",
+//                    "https://opentest.haiermoney.com:18980/sign/index/down_mod/10d3b25d1e86684ad9f673bfc7096e77",
+//                    "D:\\localFile\\contracts\\"+i+"-hello.pdf");
+//            pool.execute(downloadOneContract);
+//        }
+//        pool.shutdown();
     }
 
     public static class DownloadOneContract implements Runnable {
